e7aceabd53ae02f3d50cf5d105686aaf723d920d
mariocatu67-arch <mariocatu67@gmail.com>
2025-10-20 20:32:59 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installato');
-});
-
-self.addEventListener('fetch', event => {
-  // puoi aggiungere caching qui, per ora fa solo log
-  event.respondWith(fetch(event.request));
-});
